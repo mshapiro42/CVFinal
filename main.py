82558a73f8f7543b3d883661d26fe3824eebd631
Megan Shapiro <mshapiro@mines.edu>
2020-11-06 17:33:38 -0700

    Musical Beeps

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import cv2
 import numpy as np
+import musicalbeeps
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    player = musicalbeeps.Player(volume=0.3,
+                                 mute_output=False)
 
+    # Examples:
+    tempo = 60 # beats per minute
+    spb = 60/tempo
+    notes = ["E","D","C","D","E","E","E","D","D","D","E","G","G",
+             "E","D","C","D","E","E","E","D","E","E","E","D","C"]
+    durations = [.25,.25,.25,.25,.25,.25,.5,.25,.25,.5,.25,.25,.5,
+                 .25,.25,.25,.25,.25,.25,.25,.25,.25,.25,.25,1]
+    for i in range(len(notes)):
+        player.play_note(notes[i],spb*durations[i])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
