36a3b0e9a791ecdbef6e0a6e9e24e4005fc0920e
Megan Shapiro <mshapiro@mines.edu>
2020-11-06 17:00:14 -0700

    Init Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
