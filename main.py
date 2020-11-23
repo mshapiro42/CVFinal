6365be60b00a8f86dcdd507ccb49a16daab580d0
Megan Shapiro <mshapiro@mines.edu>
2020-11-23 14:26:05 -0700

    Updated song pdfs and jpgs

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 import musicalbeeps
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+
+def playHardCoded():
     player = musicalbeeps.Player(volume=0.3,
                                  mute_output=False)
 
@@ -15,6 +15,11 @@ if __name__ == '__main__':
     durations = [.25,.25,.25,.25,.25,.25, .5,.25,.25, .5,.25,.25,.5,
                  .25,.25,.25,.25,.25,.25,.25,.25,.25,.25,.25,.25, 1] #note lengths in beats
     for i in range(len(notes)):
-        player.play_note(notes[i],spb*durations[i])
+        player.play_note(notes[i], spb * durations[i])
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+        playHardCoded()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
